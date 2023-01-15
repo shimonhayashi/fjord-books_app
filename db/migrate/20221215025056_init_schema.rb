e67d39acaa99e0ebd2dc97f98deb10fce7e62b8d
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2023-01-15 09:45:40 +0900

    マイグレーションファイルの修正

--- db/migrate/20221215025056_init_schema.rb
+++ db/migrate/20221215025056_init_schema.rb
@@ -1,31 +1,10 @@
-class InitSchema < ActiveRecord::Migration
-  def up
+class InitSchema < ActiveRecord::Migration[6.1]
+  def change
     create_table "books" do |t|
       t.string "title"
       t.text "memo"
       t.datetime "created_at", precision: 6, null: false
       t.datetime "updated_at", precision: 6, null: false
     end
-    create_table "users" do |t|
-      t.string "email", default: "", null: false
-      t.string "encrypted_password", default: "", null: false
-      t.string "reset_password_token"
-      t.datetime "reset_password_sent_at"
-      t.datetime "remember_created_at"
-      t.datetime "created_at", precision: 6, null: false
-      t.datetime "updated_at", precision: 6, null: false
-      t.integer "postal_code"
-      t.string "prefecture_code"
-      t.string "city"
-      t.string "street"
-      t.string "other_address"
-      t.string "self_introduction"
-      t.index ["email"], name: "index_users_on_email", unique: true
-      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    end
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration, "The initial migration is not revertable"
   end
 end
