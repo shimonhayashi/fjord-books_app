b38574549d7150f7f2e30a242f25b60d43123444
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2023-01-19 09:31:12 +0900

    user.rbのvalidationを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :postal_code, presence: true
-  validates :prefecture_code, presence: true
-  validates :city, presence: true
-  validates :street, presence: true
-
   def after_confirmation
     UserMailer.welcome_email(self).deliver
   end
