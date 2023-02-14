230614801809bc2ec9a84650a5a16ffe8e21af91
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2023-02-14 16:46:12 +0900

    redirect_toの遷移先をフォローしているユーザページに変更

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
 class FollowRelationshipsController < ApplicationController
+  before_action :set_user
+
   def create
     follower = User.find_by(params[:follower_id])
     current_user.follow(params[:user_id]) if follower
-    redirect_to user_path(@current_user)
+    redirect_to user_path(@user)
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to user_path(@current_user)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
