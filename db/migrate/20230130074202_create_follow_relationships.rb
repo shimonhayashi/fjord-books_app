d1e59692dbe250a2acc6dc7d7685495157574f97
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2023-02-14 16:46:56 +0900

    マイグレーションファイル１つにまとめる／foregin_keyの設定

--- db/migrate/20230130074202_create_follow_relationships.rb
+++ db/migrate/20230130074202_create_follow_relationships.rb
@@ -1,10 +1,16 @@
 class CreateFollowRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :follow_relationships do |t|
-      t.integer :follower_id
-      t.integer :followed_id
+      t.integer :follower_id, null: false
+      t.integer :followed_id, null: false
 
       t.timestamps
     end
+
+    add_foreign_key :follow_relationships, :users, column: :follower_id
+    add_foreign_key :follow_relationships, :users, column: :followed_id
+    add_index :follow_relationships, :follower_id
+    add_index :follow_relationships, :followed_id
+    add_index :follow_relationships, %i[follower_id followed_id], unique: true
   end
 end
--- db/migrate/20230209004727_add_index_to_follow_relationships.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexToFollowRelationships < ActiveRecord::Migration[6.1]
-  def change
-    add_index :follow_relationships, :follower_id
-    add_index :follow_relationships, :followed_id
-    add_index :follow_relationships, %i[follower_id followed_id], unique: true
-  end
-end
