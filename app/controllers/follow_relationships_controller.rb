dd5f7ac22f06f074272dc5369b9ba0148e4c5019
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2023-02-17 14:40:01 +0900

    フォロー・フォロー解除が成功・失敗した場合の条件分岐、メッセージを導入

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -1,22 +1,23 @@
 # frozen_string_literal: true
 
 class FollowRelationshipsController < ApplicationController
-  before_action :set_user
-
   def create
-    follower = User.find_by(params[:follower_id])
-    current_user.follow(params[:user_id]) if follower
-    redirect_to user_path(@user)
+    if User.exists?(params[:user_id])
+      user = User.find(params[:user_id])
+      current_user.follow(params[:user_id])
+      redirect_to user_path(user), notice: 'フォローをしました'
+    else
+      redirect_to users_path, notice: 'フォローが失敗しました'
+    end
   end
 
   def destroy
-    current_user.unfollow(params[:user_id])
-    redirect_to user_path(@user)
-  end
-
-  private
-
-  def set_user
-    @user = User.find(params[:user_id])
+    if User.exists?(params[:user_id])
+      user = User.find(params[:user_id])
+      current_user.unfollow(params[:user_id])
+      redirect_to user_path(user), notice: 'フォローを解除しました'
+    else
+      redirect_to users_path, notice: 'フォローの解除が失敗しました'
+    end
   end
 end
