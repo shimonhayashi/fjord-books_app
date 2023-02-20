9f53671430b5d9073cc04a13352007bd902074f2
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2023-02-20 17:17:01 +0900

    follower,followedをadd_foreign_keyからreferenceで修正

--- db/migrate/20230130074202_create_follow_relationships.rb
+++ db/migrate/20230130074202_create_follow_relationships.rb
@@ -1,16 +1,12 @@
 class CreateFollowRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :follow_relationships do |t|
-      t.integer :follower_id, null: false
-      t.integer :followed_id, null: false
+      t.references :follower, type: :integer, null: false, index: true, foreign_key: { to_table: :users }
+      t.references :followed, type: :integer, null: false, index: true, foreign_key: { to_table: :users }
 
       t.timestamps
     end
 
-    add_foreign_key :follow_relationships, :users, column: :follower_id
-    add_foreign_key :follow_relationships, :users, column: :followed_id
-    add_index :follow_relationships, :follower_id
-    add_index :follow_relationships, :followed_id
     add_index :follow_relationships, %i[follower_id followed_id], unique: true
   end
 end
