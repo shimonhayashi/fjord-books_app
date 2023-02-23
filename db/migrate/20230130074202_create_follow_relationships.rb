7cd641880efc5b60f5790e3b6e75c86545f1bc89
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2023-02-23 12:56:07 +0900

    type,indexでデフォルト値を使うよう変更

--- db/migrate/20230130074202_create_follow_relationships.rb
+++ db/migrate/20230130074202_create_follow_relationships.rb
@@ -1,8 +1,8 @@
 class CreateFollowRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :follow_relationships do |t|
-      t.references :follower, type: :integer, null: false, index: true, foreign_key: { to_table: :users }
-      t.references :followed, type: :integer, null: false, index: true, foreign_key: { to_table: :users }
+      t.references :follower, null: false, foreign_key: { to_table: :users }
+      t.references :followed, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
