c998eae29c899a4db8b191e7e01af6afb02624e2
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2023-02-24 20:58:05 +0900

    followed_idをfollowedに変更

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class FollowRelationshipsController < ApplicationController
+  before_action :set_user
+
   def create
-    if current_user.follow(params[:user_id])
-      set_user
+    if current_user.follow(@user)
       redirect_to user_path(@user), notice: 'フォローをしました'
     else
       redirect_to users_path, alert: 'フォローに失敗しました'
@@ -11,8 +12,7 @@ class FollowRelationshipsController < ApplicationController
   end
 
   def destroy
-    if current_user.unfollow(params[:user_id])
-      set_user
+    if current_user.unfollow(@user)
       redirect_to user_path(@user), notice: 'フォローを解除しました'
     else
       redirect_to users_path, alert: 'フォローの解除に失敗しました'
@@ -20,6 +20,6 @@ class FollowRelationshipsController < ApplicationController
   end
 
   def set_user
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:user_id]) if User.exists?(params[:user_id])
   end
 end
