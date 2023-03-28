f427b616237fe4aa58c48e451311fc23c1f59cd4
shimonhayashi <hayashi.shimon@donuts.ne.jp>
2023-03-28 16:13:04 +0900

    ReportクラスとUserクラスのモデルテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    report = reports(:alice)
+    alice = users(:alice)
+    bob = users(:bob)
+
+    assert report.editable?(alice)
+    assert_not report.editable?(bob)
+  end
+
+  test '#created_on' do
+    report = reports(:alice)
+    assert_equal Date.current, report.created_on
+  end
 end
